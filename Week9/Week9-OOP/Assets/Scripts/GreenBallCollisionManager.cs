402f7f823893829c2e8a834b945a04ebf63242cd
Jeffrey571 <112986704+Jeffrey571@users.noreply.github.com>
2022-11-01 20:34:43 -0400

    Week 9 HW
    Red ball can change to random color on impact.
    
    Green Ball launches other balls towards itself.

--- Week9/Week9-OOP/Assets/Scripts/GreenBallCollisionManager.cs
+++ Week9/Week9-OOP/Assets/Scripts/GreenBallCollisionManager.cs
@@ -8,6 +8,9 @@ public class GreenBallCollisionManager : BallCollisionManager
         Debug.Log("Green Ball Collision Manager Function");
 
         // Homework: Do something interesting here
+
+        OtherBall.GetComponent<Rigidbody>().AddForce((transform.position - OtherBall.transform.position) * 1000f);
+
     }
 }
 
--- Week9/Week9-OOP/Assets/Scripts/RedBallCollisionManager.cs
+++ Week9/Week9-OOP/Assets/Scripts/RedBallCollisionManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class RedBallCollisionManager : BallCollisionManager
 {
@@ -8,6 +9,9 @@ public class RedBallCollisionManager : BallCollisionManager
         Debug.Log("Red Ball Collision Manager Function");
 
         // Homework: Do something interesting here
+
+        gameObject.GetComponent<Renderer>().material.color = new Color(Random.value, Random.value, Random.value);
+
     }
 }
 
